9614dc008f566df66da17f734cb59a6bbc22b707
Gamaiel Zavala <gamaiel@yahoo.com>
2024-05-16 14:51:15 -0600

    Build a patch that can be applied after

--- build.bat
+++ build.bat
@@ -3,8 +3,10 @@ SETLOCAL
 SET BUILD_DIR=%~DP0
 SET VERSION=1_0
 SET MM_ROM="Maniac Mansion (USA).nes"
+SET TMP_ROM="Temp.nes"
+SET BASE_ROM="Base.nes"
 SET OUT_ROM="Maniac Mansion (USA) Relocated.nes"
-SET PATCH_NAME="Maniac Mansion (USA) Relocated v%VERSION%.bps"
+SET PATCH_NAME="Maniac Mansion (USA) Relocated v%VERSION%.ips"
 SET SNARFBLASM=%BUILD_DIR%tools\snarfblasm
 SET FLIPS=tools\flips
 SET DD=tools\dd
@@ -15,6 +17,8 @@ IF "%1" == "clean" (
   ECHO Cleaning up generated files...
   DEL src\*.ips
   DEL %PATCH_NAME%
+  DEL %TMP_ROM%
+  DEL %BASE_ROM%
   DEL %OUT_ROM%
   EXIT /B
 )
@@ -35,30 +39,24 @@ IF "%1" == "patches" (
   EXIT /B
 )
 
-WHERE /Q git
-IF NOT ERRORLEVEL 1 (
-  ECHO Checking out clean roms...
-  git checkout %MM_ROM%
-)
-
 ECHO Building patches...
 CD src
+%SNARFBLASM% header.asm
 %SNARFBLASM% relocated.asm
 %SNARFBLASM% clear_banks.asm
 CD ..
 
+COPY /Y %MM_ROM% %TMP_ROM%
+
+%DD% if=%TMP_ROM% of=%BASE_ROM%
+%DD% if=%TMP_ROM% bs=16 skip=1 of=%BASE_ROM% seek=16385
+
 ECHO Patching...
-%FLIPS% src\relocated.ips %MM_ROM%
+%FLIPS% src\clear_banks.ips %BASE_ROM%
+%FLIPS% src\header.ips %TMP_ROM%
+%FLIPS% src\relocated.ips %TMP_ROM%
 
-%DD% if=%MM_ROM% of=%OUT_ROM%
-%DD% if=%MM_ROM% bs=16 skip=1 of=%OUT_ROM% seek=16385
+%DD% if=%TMP_ROM% of=%OUT_ROM%
+%DD% if=%TMP_ROM% bs=16 skip=1 of=%OUT_ROM% seek=16385
 
-WHERE /Q git
-IF NOT ERRORLEVEL 1 (
-  git checkout %MM_ROM%
-)
-
-ECHO Clearing banks...
-%FLIPS% src\clear_banks.ips %OUT_ROM%
-
-%FLIPS% -c --bps %MM_ROM% %OUT_ROM% %PATCH_NAME%
+%FLIPS% -c %BASE_ROM% %OUT_ROM% %PATCH_NAME%
